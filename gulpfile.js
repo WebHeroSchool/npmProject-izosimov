9f5bd0fb4238e99283877a59f9b18bcdea0eb8b7
Артём Изосимов <1996loko@rambler.ru>
2018-11-03 00:17:07 +0300

    ENV, Переменные окружения

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+const env = require('gulp-env');
+const clean = require('gulp-clean');
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
@@ -22,6 +24,16 @@ const paths = {
     }
 }
 
+env({
+    file: '.env',
+    type: 'ini'
+})
+
+gulp.task('clean', function () {
+    gulp.src('build', {read: false})
+        .pipe(clean());
+});
+
 gulp.task('build-js', () => {
     gulp.src(paths.src.scripts)
         .pipe(sourcemaps.init())
@@ -29,7 +41,7 @@ gulp.task('build-js', () => {
             .pipe(babel({
                 presets: ['@babel/env']
             }))
-            .pipe(gulpif(process.env.NODE_ENV === 'prodaction', uglify()))
+            .pipe(gulpif(process.env.NODE_ENV === 'production', uglify()))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.build.scripts));
 });
@@ -38,7 +50,7 @@ gulp.task('build-css', () => {
     gulp.src(paths.src.styles)
         .pipe(sourcemaps.init())
             .pipe(concat(paths.buildNames.styles))
-            .pipe(gulpif(process.env.NODE_ENV === 'prodaction', cssnano()))
+            .pipe(gulpif(process.env.NODE_ENV === 'production', cssnano()))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.build.styles));
 });
@@ -58,5 +70,7 @@ gulp.task('browser-sync', () => {
 gulp.task('js-watch', ['build-js'], () => browserSync.reload());
 gulp.task('css-watch', ['build-css'], () => browserSync.reload());
 
+gulp.task('clean-build', ['clean']);
 gulp.task('prod', ['build']);
 gulp.task('dev', ['build', 'browser-sync']);
+gulp.task('default', ['dev']);
