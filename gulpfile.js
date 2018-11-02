9195fc9f9ba81bdfbae52ee02bd852feaa1f51a6
Артём Изосимов <1996loko@rambler.ru>
2018-11-02 11:02:26 +0300

    Конкатенация и минификация CSS-файлов

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,21 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const cssnano = require('gulp-cssnano')
 
 gulp.task('build', () => {
-    gulp.src('styles.css').pipe(gulp.dest('build/styles'));
-    gulp.src(['scripts.js', 'index.js'])
+    gulp.src('scripts/*.js')
         .pipe(concat('index.js'))
         .pipe(babel({
             presets: ['@babel/env']
         }))
         .pipe(uglify())
         .pipe(gulp.dest('build/scripts'));
+});
+
+gulp.task('build-css', () => {
+    gulp.src('styles/*.css')
+        .pipe(concat('index.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('build/styles'));
 });
