5b1119e60ce2f26308bec054b1223b0dcd98cc29
Артём Изосимов <1996loko@rambler.ru>
2018-11-02 12:32:25 +0300

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 
 gulp.task('build', () => {
+    gulp.src('scripts.js').pipe(babel({presets: ['@babel/env']})).pipe(gulp.dest('build/scripts'));
+});
+
+gulp.task('build-css', () => {
     gulp.src('styles.css').pipe(gulp.dest('build/styles'));
-    return gulp.src('scripts.js').pipe(babel({presets: ['@babel/env']})).pipe(gulp.dest('build/scripts'));
 });
